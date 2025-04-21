c74653d461c37b26ec90ab026cde1d833433ed88
yuandapeng <18852369945@163.com>
2025-04-21 11:13:40 +0800

    feat: 添加全局字体

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] }); // latin 加载拉丁字母
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
