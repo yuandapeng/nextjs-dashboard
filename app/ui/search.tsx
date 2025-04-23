35fa8e4ba6ff7d2e566c29778fc107b4642dc461
yuandapeng <18852369945@163.com>
2025-04-23 11:36:21 +0800

    feat: 在搜索组件中添加防抖功能以优化搜索体验

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -8,7 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -16,7 +18,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
