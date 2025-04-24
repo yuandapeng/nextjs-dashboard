bd30ebba6f3c8ff9487c73701745af5cc97be489
yuandapeng <18852369945@163.com>
2025-04-24 11:29:47 +0800

    feat: 增强发票创建表单的验证和错误处理，优化用户体验

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,28 +5,51 @@ import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  // 通过zod的string方法验证customerId为字符串类型
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  // 通过gt方法验证金额大于0
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  // 通过zod的enum方法验证status为枚举类型
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
-  console.log('customerId', customerId);
-  console.log('amount', amount);
-  console.log('status', status);
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
 
@@ -36,6 +59,9 @@ export async function createInvoice(formData: FormData) {
     await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
   } catch (error) {
     console.error('Error creating invoice:', error);
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
   }
 
   // 重新验证发票列表页面
@@ -73,5 +99,3 @@ export async function deleteInvoice(id: string) {
 
   revalidatePath('/dashboard/invoices');
 }
-
-
