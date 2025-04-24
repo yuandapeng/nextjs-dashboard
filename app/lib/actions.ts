9234ef936e52a203db90e70eb1a70a6fe1639517
yuandapeng <18852369945@163.com>
2025-04-24 10:04:49 +0800

    feat: 添加发票删除功能及相关按钮

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -62,5 +62,10 @@ export async function updateInvoice(id: string, formData: FormData) {
   }
 
 
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+
+  revalidatePath('/dashboard/invoices');
+}
 
 
